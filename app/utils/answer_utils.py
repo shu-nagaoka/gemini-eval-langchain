afbb1e64610dbead1355e515799a6c5d985a85fa
shu-nagaoka <shu.nagaoka@g.softbank.co.jp>
2024-06-05 08:14:59 +0900

    change files

--- app/utils/answer_utils.py
+++ app/utils/answer_utils.py
@@ -2,8 +2,6 @@ from typing import Any, List, Mapping, Optional
 from langchain.llms.base import LLM
 from langchain.chains.question_answering import load_qa_chain
 import google.generativeai as genai
-from dotenv import load_dotenv
-load_dotenv()
 
 # Gemini API用のカスタムLLMクラス
 class GeminiLLM(LLM):
