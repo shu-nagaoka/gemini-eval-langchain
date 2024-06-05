41aed33a7231640a458eaec483409f04e1a866ef
shu-nagaoka <shu.nagaoka@g.softbank.co.jp>
2024-06-05 10:53:25 +0900

    change files

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,6 +1,8 @@
 import vertexai
 import google.generativeai as genai
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 PROJECT_ID = os.getenv('PROJECT_ID')
