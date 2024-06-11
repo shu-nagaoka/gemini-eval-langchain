8ff7ab53b407291d66cd10733056adaf83f8e80a
shu-nagaoka <shu.nagaoka@g.softbank.co.jp>
2024-06-11 20:46:34 +0900

    EMBEDMODELを環境変数化しつつGCSバケットにPDFを保存処理

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -7,35 +7,51 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-client = OpenAI()
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PERSIST_DIRECTORY = os.path.join(os.path.dirname(__file__), '..', 'db')
+EMBED_MODEL = os.getenv('EMBED_MODEL')
 
-embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+# GEMINIのEMBEDDINGモデルを使用する場合
+# GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+# OPENAIのEMBEDDINGモデルを使用する場合
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+client = OpenAI()
+embeddings = OpenAIEmbeddings(model=EMBED_MODEL)
 vectorstore = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings)
+
+import logging
+from datetime import datetime
+current_date = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+log_filename = f"logs-{current_date}.log"
+logging.basicConfig(filename=log_filename,
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def format_eval_summary(eval_summary):
     return "\n".join([f"{key}: {value}" for key, value in eval_summary.items()])
 
 async def get_answer_from_db(query):
     """既存のベクトルDBから回答を取得する"""
-    print(f"get_answer_from_db関数: query={repr(query)}")
+    logging.info(f"get_answer_from_db関数: {query}")
     results = vectorstore.similarity_search(query, k=5)
-    print(f"get_answer_from_db関数: results={repr(results)}")
+    logging.info(f"get_answer_from_db関数:{results}")
 
     if results:
-        context = results[0].page_content
+        # context = results[0].page_content
+        contexts = [result.page_content for result in results]
+        combined_context = "\n\n".join(contexts)
         model = genai.GenerativeModel('gemini-1.5-flash-001')
         response = model.generate_content(
             f"あなたは有能なアシスタントです。\n\n"
             f"以下のコンテキストに基づいて質問に答えてください。\n\n"
-            f"コンテキスト: {context}\n\n"
+            f"コンテキスト: {combined_context}\n\n"
             f"質問: {query}"
         )
 
-        eval_summary, formatted_response = await evaluate_rag(query, context, response.text.strip(), instruction="事実に基づく回答のみを提供してください。")
-        print("評価結果サマリー:", eval_summary)
-        print("評価結果テーブル(構造化済み):", formatted_response)
+        eval_summary, formatted_response = await evaluate_rag(query, combined_context, response.text.strip(), instruction="事実に基づく回答のみを提供してください。")
+        # logging.info(f"combined_contexts: {combined_context}")
+        logging.info(f"評価結果サマリー: {eval_summary}")
+        logging.info(f"評価結果テーブル(構造化済み): {formatted_response}")
         return response.text.strip(), eval_summary, format_eval_summary(eval_summary), formatted_response.strip()
     else:
         return "データベースに一致するドキュメントがありませんでした。", None, None
--- app/utils/processing.py
+++ app/utils/processing.py
@@ -1,14 +1,25 @@
 import io
 import os
 import anyio
-from utils.pdf_utils import check_existing_pdf, extract_chunks_from_pdf, save_temp_pdf, move_pdf_to_directory
+from utils.pdf_utils import extract_chunks_from_pdf, save_temp_pdf, upload_to_gcs
 from utils.db_utils import get_answer_from_db, vectorstore
 from utils.answer_utils import get_answer_from_documents
 import google.generativeai as genai
+import tempfile
+from google.cloud import storage
 from dotenv import load_dotenv
 load_dotenv()
 
 PDF_DIRECTORY = os.path.join(os.path.dirname(__file__), '..', 'pdf')
+GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME')
+
+import logging
+from datetime import datetime
+current_date = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+log_filename = f"logs-{current_date}.log"
+logging.basicConfig(filename=log_filename,
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def get_pdf_path(pdf_file):
     """PDFファイルのパスを取得する"""
@@ -23,6 +34,13 @@ def add_chunks_to_vectorstore(chunks):
     for i in range(0, len(chunks), chunk_size):
         vectorstore.add_documents(chunks[i:i+chunk_size])
 
+def check_existing_pdf_in_gcs(bucket_name, blob_name):
+    """GCSにすでにPDFがあるかを確認する"""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    return blob.exists()
+
 async def async_process_pdf(pdf_file, query):
     """PDFを処理し、クエリに対する回答を生成する (非同期版)"""
     if pdf_file is None:
@@ -35,12 +53,26 @@ async def async_process_pdf(pdf_file, query):
     else:
         temp_pdf_path = pdf_path
 
-    if check_existing_pdf(temp_pdf_path):
+    destination_blob_name = os.path.basename(temp_pdf_path)
+    logging.info(f"添付されたファイル名: {destination_blob_name}")
+
+    # GCSバケット内にPDFファイルが存在するかを確認
+    if check_existing_pdf_in_gcs(GCS_BUCKET_NAME, destination_blob_name):
         db_answer, eval_summary, eval_summary_formatted, eval_table = await get_answer_from_db(query)
-        return "このPDFはすでに処理済みです。", db_answer, eval_summary_formatted, eval_table
+        return "このPDFはGCSバケットに保存されており、すでに処理済みです。", db_answer, eval_summary_formatted, eval_table
     else:
-        pdf_path = move_pdf_to_directory(temp_pdf_path, PDF_DIRECTORY)
-        chunks = extract_chunks_from_pdf(pdf_path)
+        # GCSへのuploadの開始
+        logging.info(f"{GCS_BUCKET_NAME}へアップロードを開始します。")
+        upload_to_gcs(temp_pdf_path, GCS_BUCKET_NAME, destination_blob_name)
+        logging.info(f"{GCS_BUCKET_NAME}へアップロードが完了しました。")
+
+        # GCSからPDFをダウンロードしてチャンクを抽出
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(GCS_BUCKET_NAME)
+        blob = bucket.blob(destination_blob_name)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+            blob.download_to_filename(temp_file.name)
+            chunks = extract_chunks_from_pdf(temp_file.name)
         add_chunks_to_vectorstore(chunks)
         return "新規PDFとしてベクトル化が完了しました。", None, None, None
 
